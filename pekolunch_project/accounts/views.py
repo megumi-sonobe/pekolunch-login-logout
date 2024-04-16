6a50b6272cf9c978b9b73ad58e75c0715cd6c843
園部恵 <sonobemegumi@sonobemeguminoMacBook-Pro.local>
2024-04-16 15:52:29 +0900

    ログインログアウト成功

--- pekolunch_project/accounts/views.py
+++ pekolunch_project/accounts/views.py
@@ -3,6 +3,8 @@ from django.views.generic.edit import CreateView,FormView
 from django.views.generic.base import TemplateView,View
 from.forms import RegistForm,UserLoginForm
 from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 
 class HomeView(TemplateView):
@@ -18,16 +20,29 @@ class UserLoginView(FormView):
     form_class = UserLoginForm
     
     def post(self,request,*args,**kwargs):
-        email = request.POST['email']
-        password = request.POST['password']
-        user = authenticate(email=email,password=password)
-        if user is not None and user.is_active:
-            login(request,user)
-        return redirect('accounts:home')
+        form = self.form_class(request.POST)
+        
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = authenticate(email=email,password=password)
             
+            if user is not None and user.is_active:
+                login(request,user)
+                return redirect('accounts:home')
+            else:
+                messages.warning(request,'メールアドレスかパスワードが間違っています')
+        else:
+            messages.warning(request,'入力された情報に問題があります。正しいメールアドレスとパスワードを入力してください。')
+                
+        return render(
+            request,self.template_name,{'form':form})
+            
+
 
 class UserLogoutView(View):
     def get(self,request,*args,**kwargs):
         logout(request)
+        messages.success(request,'ログアウトしました')
         return redirect('accounts:user_login')
     
