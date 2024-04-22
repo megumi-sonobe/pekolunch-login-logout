6101f797d78a2ad9be767391e0308b8d68597f8f
megumi-sonobe <megu-rak26@gmail.com>
2024-04-22 15:44:18 +0900

    models.py作成

--- pekolunch_project/meal_planner/models.py
+++ pekolunch_project/meal_planner/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from accounts.models import Users
+from django.utils import timezone
 
-# Create your models here.
+class MealPlan(models.Model):
+    user = models.ForeignKey(Users,on_delete=models.CASCADE)
+    staple_recipe = models.ForeignKey(Recipe,related_name='主食',on_delete=models.CASCADE)
+    main_recipe = models.ForeignKey(Recipe,related_name='主菜',on_delete=models.CASCADE)
+    side_recipe = models.ForeignKey(Recipe,related_name='副菜',on_delete=models.CASCADE)
+    soup_recipe = models.ForeignKey(Recipe,related_name='汁物',on_delete=models.CASCADE)
+    meal_date = models.DateField()
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    
