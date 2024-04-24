b6c92f4f8a2dbf6482224dd08a3752a2abdf661c
megumi-sonobe <megu-rak26@gmail.com>
2024-04-24 17:52:32 +0900

    献立自動提案の条件追加

--- pekolunch_project/meal_planner/service.py
+++ pekolunch_project/meal_planner/service.py
@@ -1,9 +1,7 @@
 import csv
-from datetime import timedelta
 from django.utils import timezone
 from recipes.models import Recipe,FoodCategory
 from meal_planner.models import MealPlan
-from random import choice
 
 
 #CSVで食材データを読み込んでルールに当てはめる
@@ -23,24 +21,51 @@ def load_food_categories_from_csv(file_path):
     
 def apply_rules(recipes,today,user):
     rules = load_food_categories_from_csv('path_to_your_file.csv')
-    
-    exclude_food_categories = []
-    
-    for category_name,rule in rules.items():
-        if rule['exclude_next_day']:
-            exclude_food_categories.append(category_name)
-        elif rule['exclude_next_3_days']:
-            exclude_food_categories.append(category_name)
-            
+    exclude_food_categories = [category for category,rule in rules.items() if rule['exclude_next_day'] or rule['exclude_next_3_days']]
+         
     return recipes.exclude(food_categories__food_category_name__in=exclude_food_categories)
 
 
-def get_eligible_recipes(cls, user):
+def get_eligible_recipes(user,previous_meal_plans):
     cooking_time_setting = user.cooking_time_min
-    
+#ユーザーが指定した調理時間に基づく
     if cooking_time_setting == 30:
-        return Recipe.objects.all()
+        base_queryset =  Recipe.objects.all()
     elif cooking_time_setting == 20:
-        return Recipe.objects.filter(cooking_time_min__lte=20)
+        base_queryset = Recipe.objects.filter(cooking_time_min__lte=20)
     
-    return Recipe.objects.filter(cooking_time_min__lte=cooking_time_setting)
+    else:
+        base_queryset = Recipe.objects.filter(cooking_time_min__lte=cooking_time_setting)
+        
+# #評価が高い順に選ばれる
+#     base_queryset = base_queryset.order_by('-average_evaluation')
+
+
+#主食、主菜、副菜から１品ずつ選択
+    staple_recipe = base_queryset.filter(menu_category=1).order_by('-average_evaluation').first()
+    main_recipe = base_queryset.filter(menu_category=2).order_by('-average_evaluation').first()
+    side_recipe = base_queryset.filter(menu_category=3).order_by('-average_evaluation').first()
+   
+#20日間は同じレシピを使用しない
+    if previous_meal_plans:
+        last_used_staple = [plan.staple_recipe.id for plan in previous_meal_plans[-20:]]
+        last_used_main = [plan.main_recipe.id for plan in previous_meal_plans[-20:]]
+        last_used_side = [plan.side_recipe.id for plan in previous_meal_plans[-20:]]
+        
+        last_used_recipes = last_used_staple + last_used_main + last_used_side
+        base_queryset = base_queryset.exclude(id__in=last_used_recipes)
+
+#同じ調理法のレシピを使用しない   
+    today = timezone.now().date()
+    today_used_methods = []
+    
+    for plan in previous_meal_plans:
+        if plan.meal_date == today:
+            today_used_methods.append(plan.staple_recipe.cooking.method)
+            today_used_methods.append(plan.main_recipe.cooking.method)
+            today_used_methods.append(plan.side_recipe.cooking.method)
+            
+    base_queryset = base_queryset.exclude(cooking_method__in=today_used_methods)
+    
+    return [staple_recipe, main_recipe, side_recipe]
+       
