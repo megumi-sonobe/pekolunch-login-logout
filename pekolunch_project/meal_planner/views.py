6a7ac90966371a8c02c3e581584797d8c71ca11d
megumi-sonobe <megu-rak26@gmail.com>
2024-04-25 15:02:57 +0900

    home.html修正

--- pekolunch_project/meal_planner/views.py
+++ pekolunch_project/meal_planner/views.py
@@ -1,1 +1,32 @@
 from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from .forms import MealPlanForm
+from datetime import timedelta
+from .models import MealPlan
+
+
+@login_required
+def create_meal_plans(request):
+    if request.method == 'POST':
+        form  = MealPlanForm(request.POST)
+        if form.is_valid():
+            meal_date = form.cleaned_data['meal_date']
+            
+            for _ in range(7):
+                
+                if not MealPlan.objects.filter(user=request.user,meal_date=meal_date).exists():
+                    meal_plan = form.save(commit=False)
+                    meal_plan.user = request.user
+                    meal_plan.save()
+                    
+                meal_date += timedelta(days=1)
+                    
+            return redirect('meal_planner_edit', meal_date=(meal_date - timedelta(days=1)).strftime('%Y-%m-%d'))
+
+    else:
+        form = MealPlanForm()
+        
+    return render(request,'home.html',{'form':form})
+
+            
+            
