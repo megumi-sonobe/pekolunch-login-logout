3afa48761a7fa55d32917c0d0e83fc2996c0747e
megumi-sonobe <megu-rak26@gmail.com>
2024-05-17 10:33:07 +0900

    food_categoryスクロール

--- pekolunch_project/pekolunch_project/settings.py
+++ pekolunch_project/pekolunch_project/settings.py
@@ -128,9 +128,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    STATIC_DIR
+    os.path.join(BASE_DIR, 'static'),
 ]
-# STATIC_ROOT = os.path.join(BASE_DIR,'static/')
+STATIC_ROOT = os.path.join(BASE_DIR,'static_root')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
